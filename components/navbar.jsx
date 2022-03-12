d878a8cd9d6b5c5c013c53d0e0a3bb7590787a52
urdreamboi <its.husin@gmail.com>
2022-03-12 20:44:40 +0700

    udahan udah jadi

---
+++ components/navbar.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+
+export default function Navbar() {
+  const [navbarShown, showNavbar] = useState(false);
+
+  return (
+    <>
+      {/* Desktop */}
+      <nav className="font-karla hidden w-full bg-white p-3 lg:block">
+        <div className="mx-auto flex w-full max-w-[1200px] items-center justify-between">
+          <div>
+            <img src="/logo.svg" />
+          </div>
+
+          <div className="text-neutral-dark-grayish-violet font-bold">
+            <a href="/" className="hover:text-neutral-very-dark-violet pr-3">
+              HOW WE WORK
+            </a>
+            <a href="/" className="hover:text-neutral-very-dark-violet pr-3">
+              BLOG
+            </a>
+            <a href="/" className="hover:text-neutral-very-dark-violet pr-3">
+              ACCOUNT
+            </a>
+            <button className="font-karla text-neutral-very-dark-violet hover:bg-neutral-very-dark-violet border-neutral-very-dark-violet ml-5 border-2 px-6 py-1 font-bold hover:text-white">
+              VIEW PLANS
+            </button>
+          </div>
+        </div>
+      </nav>
+
+      {/* Mobile */}
+      <nav className="font-karla fixed top-0 max-h-[63px] z-20 w-full bg-white p-3 lg:hidden">
+        <div className="flex w-full max-w-[1200px] items-center justify-between">
+          <div>
+            <img src="/logo.svg" />
+          </div>
+
+          <div>
+            <button onClick={() => showNavbar(true)}>
+              <img src="/icons/icon-hamburger.svg" />
+            </button>
+          </div>
+        </div>
+
+        {/* Hidden part */}
+        <div
+          className={`fixed left-0 top-0 h-screen w-full bg-white ${
+            navbarShown ? "" : "hidden"
+          } z-20`}>
+          <div
+            className={` mx-auto flex h-[63px] w-full max-w-[1200px] items-center justify-between p-3`}>
+            <div>
+              <img src="/logo.svg" />
+            </div>
+
+            <div>
+              <button onClick={() => showNavbar(false)}>
+                <img src="/icons/icon-close.svg" />
+              </button>
+            </div>
+          </div>
+
+          <div
+            className={`bg-neutral-very-dark-violet h-full text-2xl font-bold text-white`}>
+            <div className=" flex flex-col items-center">
+              <a href="/" className="my-3 pt-5">
+                HOW WE WORK
+              </a>
+              <a href="/" className="my-3">
+                BLOG
+              </a>
+              <a href="/" className="my-3">
+                ACCOUNT
+              </a>
+              <button className="font-karla border-2 border-white mt-3 w-4/5 py-2 font-bold">
+                VIEW PLANS
+              </button>
+            </div>
+            <img className="absolute bottom-0 w-full" src="/bg/bg-pattern-mobile-nav.svg" />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      primary: {
-        "dark-violet": "hsl(256, 26%, 20%)",
-        "grayish-blue": "hsl(216, 30%, 68%)",
+    extend: {
+      colors: {
+        primary: {
+          "dark-violet": "hsl(256, 26%, 20%)",
+          "grayish-blue": "hsl(216, 30%, 68%)",
+        },
+        neutral: {
+          "very-dark-violet": "hsl(270, 9%, 17%)",
+          "dark-grayish-violet": "hsl(273, 4%, 51%)",
+          "very-light-gray": "hsl(0, 0%, 98%)",
+        },
       },
-      neutral: {
-        "very-dark-violet": "hsl(270, 9%, 17%)",
-        "dark-grayish-violet": "hsl(273, 4%, 51%)",
-        "very-light-gray": "hsl(0, 0%, 98%)",
+      fontFamily: {
+        dm: ["DM Serif Display"],
+        karla: ["Karla"],
       },
-    },
-    fontFamily: {
-      dm: ["DM Serif Display", "serif"],
-      karla: ["Karla", "sans-serif"],
     },
   },
   plugins: [],
