8a0d19eb89c4d0baee6dfb8457a42f1da8a03dbd
urdreamboi <its.husin@gmail.com>
2022-03-14 17:02:02 +0700

    Updates

--- _next/static/fcr9tVu-C3nyN_DPNZxn0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-efaabaf57d970dc9.js","static/chunks/d7eeaac4-d9c60be2f31f2745.js","static/chunks/pages/index-f704da1e927050bc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
