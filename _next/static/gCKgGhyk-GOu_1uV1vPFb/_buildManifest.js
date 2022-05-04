a8964bcd961d14a203b1ec751ce22728875fd493
husinzz <its.husin@gmail.com>
2022-05-05 02:28:03 +0700

    Updates

---
+++ _next/static/gCKgGhyk-GOu_1uV1vPFb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-efaabaf57d970dc9.js","static/chunks/d7eeaac4-d9c60be2f31f2745.js","static/chunks/pages/index-7eb95b9f0f27f1e0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
